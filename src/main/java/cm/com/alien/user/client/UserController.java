8fe95793420a5f104b025c34777dd0d13816bab0
Howell <th15817161961@gmail.com>
2017-07-16 23:47:29 +0800

    Initial commit

---
+++ src/main/java/cm/com/alien/user/client/UserController.java
@@ -0,0 +1,39 @@
+package cm.com.alien.user.client;
+
+import org.mockito.cglib.beans.BeanMap;
+import org.spring.boot.thrift.client.annotation.ThriftClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Howell on 17/1/11.
+ */
+@RestController
+public class UserController {
+
+    @ThriftClient(serviceId = "thriftserver", path = "/userService")
+    UserService.Client UserServiceClient;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/userLogin")
+    public Object userService() throws Exception {
+        Map<String, Object> o = new HashMap<>();
+        UserReq userReq = new UserReq();
+        userReq.setUserPasswd("123456");
+        userReq.setUserId("123456");
+        userReq.setUserIp("123456");
+        userReq.setUserMac("123456");
+        userReq.setBaseReq(new BaseReq().setIp("123456").setKeep("123456"));
+        UserServiceClient.userLogin(userReq);
+        o = BeanMap.create(UserServiceClient.userLogin(userReq).getBaseResp());
+        return o;
+    }
+
+
+
+
+
+}
